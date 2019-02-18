c3f5e34e00b6aa2270c6abfb75e4384bfbf69bf0
Wojciech Pawlak <wojciechpawlak@gmail.com>
2019-02-18 23:23:47 +0100

    Add 65536-size calls to dataset generation shell script

--- generate_datasets.sh
+++ generate_datasets.sh
@@ -2,7 +2,9 @@
 
 # American/Bermudan fixed-rate (coupon) bonds
 ./build/RandomDataGenerator -n 10000 -p ./data/
+./build/RandomDataGenerator -n 65536 -p ./data/
 ./build/RandomDataGenerator -n 100000 -p ./data/
 # European zero-coupon bonds
 ./build/RandomDataGenerator -n 10000 -e -c -p ./data/
+./build/RandomDataGenerator -n 65536 -e -c -p ./data/
 ./build/RandomDataGenerator -n 100000 -e -c -p ./data/
