6749df1e5520ab0a59a4db4d2fd708598d113ceb
Wojciech Pawlak <wojciechpawlak@gmail.com>
2019-02-18 22:58:25 +0100

    Fix wrong naming of Makefile rule for compilation for GTX780

--- test_build_gtx780.sh
+++ test_build_gtx780.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cd ./seq/
-./test.sh compile_GTX780
+./test.sh compile_gtx780
 cd ..
 cd ./cuda-option/
-./test.sh compile_GTX780
+./test.sh compile_gtx780
 cd ..
 cd ./cuda-multi/
-./test.sh compile_GTX780
+./test.sh compile_gtx780
 cd ..
 cd ./test/
-make compile_GTX780
+make compile_gtx780
 make compare
 cd ..
 cd ./data-generator/
-make compile_GTX780
+make compile_gtx780
 cd ..
