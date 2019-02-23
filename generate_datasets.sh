0a39d78e6b858e96a74f9f062e7a07f8371897ff
Wojciech Pawlak <wojciechpawlak@gmail.com>
2019-02-23 02:49:21 +0100

    Update and unify test shell scripts for testing with new types of datasets

--- generate_datasets.sh
+++ generate_datasets.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # American/Bermudan fixed-rate (coupon) bonds
-./build/RandomDataGenerator -n 10000 -p ./data/
-./build/RandomDataGenerator -n 65536 -p ./data/
-./build/RandomDataGenerator -n 100000 -p ./data/
+# ./build/RandomDataGenerator -n 10000 -p ./data/
+# ./build/RandomDataGenerator -n 65536 -p ./data/
+# ./build/RandomDataGenerator -n 100000 -p ./data/
 # European zero-coupon bonds
-./build/RandomDataGenerator -n 10000 -e -c -p ./data/
-./build/RandomDataGenerator -n 65536 -e -c -p ./data/
-./build/RandomDataGenerator -n 100000 -e -c -p ./data/
+# ./build/RandomDataGenerator -n 10000 -e -c -p ./data/
+# ./build/RandomDataGenerator -n 65536 -e -c -p ./data/
+# ./build/RandomDataGenerator -n 100000 -e -c -p ./data/
+
+./build/RandomDataGenerator -t 0 -n 1000 -p ./data/
+./build/RandomDataGenerator -t 0 -n 3000 -p ./data/
+./build/RandomDataGenerator -t 0 -n 5000 -p ./data/
+
+./build/RandomDataGenerator -t 1 -n 100000 -p ./data/
+./build/RandomDataGenerator -t 1 -n 100000 -p ./data/ --isHeightNormalDist
+./build/RandomDataGenerator -t 1 -n 100000 -p ./data/ --isWidthNormalDist
+
+./build/RandomDataGenerator -t 4 -n 100000 -p ./data/ -s 1
+./build/RandomDataGenerator -t 4 -n 100000 -p ./data/ -s 1 --inverseSkewed
